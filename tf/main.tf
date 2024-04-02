386ada7472cdfbf76be30fbb6c784c5e91afae33
Jeff Katzy <jeffreyerickatz@gmail.com>
2024-04-02 12:27:34 -0700

    update main.tf to remove extra policy

--- tf/main.tf
+++ tf/main.tf
@@ -51,23 +51,3 @@ resource "aws_iam_role_policy_attachment" "ecr_read_only" {
  role = aws_iam_role.ec2_ecr_role.name
  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
-
-resource "aws_iam_policy" "ecr_custom_read_only" {
-  name        = "ecr_custom_read_only"
-  description = "Policy that allows ECR pull images"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = ["ecr:GetAuthorizationToken",
-        "ecr:BatchGetImage",
-         "ecr:BatchCheckLayerAvailability", 
-         "ecr:GetDownloadUrlForLayer"]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-
