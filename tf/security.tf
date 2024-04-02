a9aa75343a7101c285decd4eb420192d8bcd256a
Jeff Katzy <jeffreyerickatz@gmail.com>
2024-04-02 12:24:58 -0700

    add security tf properly

--- tf/security.tf
+++ tf/security.tf
@@ -1,4 +1,5 @@
 resource "aws_security_group" "http_backend_security" {
+  name = "web access backend security permissions"
   ingress {
     from_port   = 80
     to_port     = 80
@@ -8,6 +9,7 @@ resource "aws_security_group" "http_backend_security" {
 }
 
 resource "aws_security_group" "ssh_backend_security" {
+    name = "aws ssh security group permissions"
 
     ingress {
         cidr_blocks = [
